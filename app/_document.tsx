7a5a3c2251e36620936baa4c08bc974bca532351
sazzizzback <saugatupreti22@gmail.com>
2025-11-28 19:22:09 +1100

    Added Google AdSense integration for web: - Removed old web/index.html override - Added new app/_document.tsx for AdSense script injection - Ensured Expo Router uses proper document structure

---
+++ app/_document.tsx
@@ -0,0 +1,20 @@
+import { Html, Head, Main, NextScript } from 'expo-router';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        {/* Google AdSense */}
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4177602071699578"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
